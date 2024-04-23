0d2551320a8b85d619321a27ad71a5fba25a1004
Daniel Kelly <me@danielkelly.io>
2024-04-23 17:02:25 -0500

    style

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="h-screen flex items-center justify-center -mt-24">
-    <h1 class="text-2xl">Domain Driven Design - Example with Nuxt Layers</h1>
+    <h1 class="text-2xl text-center p-5">
+      Domain Driven Design - Example with Nuxt Layers
+    </h1>
   </div>
 </template>
